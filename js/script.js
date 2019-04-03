5efb960bc1f9657da43fd9a7bdd063931585d3bb
Alex Bondarenko <alexandr96b@gmail.com>
2019-04-03 15:11:59 +0300

    add section partners

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
 $(document).ready(function () {
 
+$('.b-partners-slider').slick({
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  fade: false,
+  variableWidth: false,
+  adaptiveHeight: true,
+  arrows: false,
+  dots: true,
+  responsive: [
+  {
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
 
-$('.b-prepack-next').on("click", function() {
-    $('.b-prepack-slider__big').slick("slickNext"); 
-})
-$('.b-prepack-prev').on("click", function() {
-    $('.b-prepack-slider__big').slick("slickPrev"); 
-})
 
 $('.b-foto-slider').slick({
   slidesToShow: 1,
@@ -16,7 +45,6 @@ $('.b-foto-slider').slick({
   variableWidth: true,
   adaptiveHeight: true,
   arrows: true,
-  centerPadding: '40px',
   nextArrow: '<button class="b-arrow b-arrow__next">→</button>',
   prevArrow: '<button class="b-arrow b-arrow__prev">←</button>',
   focusOnSelect: true,
@@ -86,6 +114,15 @@ $('.b-about-slider').slick({
   // instead of a settings object
 ]
 });
+
+
+$('.b-prepack-next').on("click", function() {
+    $('.b-prepack-slider__big').slick("slickNext"); 
+})
+$('.b-prepack-prev').on("click", function() {
+    $('.b-prepack-slider__big').slick("slickPrev"); 
+})
+
 $('.b-prepack-slider__big').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
